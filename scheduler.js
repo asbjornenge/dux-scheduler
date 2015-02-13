f19a5fc26fa6db327821bae8e48542d7161106bf
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-02-13 15:04:19 +0100

    Using async lib and lots of improvements around applying. Works with updated cccf-diff looking at all properties

--- scheduler.js
+++ scheduler.js
@@ -1,81 +1,52 @@
 var cccf    = require('cccf')
 var cdiff   = require('cccf-diff')
-var scale   = require('cccf-scale')
-var chost   = require('cccf-host-basic')
-var cdi     = require('cccf-docker-instructions')
 var clone   = require('clone')
-var path    = require('path')
-var chpr    = require('child_process')
+var async   = require('async')
 var mapper  = require('./scheduler-mapper')
 var utils   = require('./scheduler-utils')
+var autils  = require('./scheduler-utils-async')
 
 var scheduler = {
 
     working       : false,
     working_timer : null,
 
-    exec : function(instruction, callback) {
-        var color = utils.pickInstructionColor(instruction) 
-        var child = chpr.exec(instruction)
-        child.stdout.on('data', function(data) { process.stdout.write(color(data)) })
-        child.stderr.on('data', function(data) { process.stderr.write(data) })
-        child.on('close', function() { 
-            if (typeof callback === 'function') callback() 
-        })
-    },
-
     apply : function(state, current_containers) {
         if (scheduler.working) return
         scheduler.working = true
-        scheduler.working_timer = setTimeout(function() {
-            console.error('Scheduler has been working for a very long time.')
-        }, 600000)
         var _mapper = mapper(state, current_containers) 
 
         unified_current_containers   = _mapper.unifyContainers(current_containers)
         var unified_state_containers = _mapper.unifyContainers(state.containers) 
         var diff                     = _mapper.applyHosts(cdiff(unified_current_containers, unified_state_containers))
 
-        console.log(diff)
-        process.exit(0)
+        scheduler.applyDiffAsync(state, diff)
+    },
 
-        // Finish 
+    applyDiffAsync : function(state, diff) {
 
-        var numUpdates  = addWithHost.length + diff.remove.length
-        var numFinished = 0
-        var finish_checker = function() {
-            numFinished += 1
-            if (numUpdates > numFinished) return
-            scheduler.working = false
-            clearTimeout(scheduler.working_timer)
-        }
-        if (numUpdates == 0) finish_checker()
+        var kill = async.compose(autils.execInstruction.bind({i:'kill'}), autils.queryHostVersion.bind({hosts:state.hosts}))
+        var rm   = async.compose(autils.execInstruction.bind({i:'rm'}),   autils.queryHostVersion.bind({hosts:state.hosts}))
+        var run  = async.compose(autils.execInstruction.bind({i:'run', exclude : ['scale','host']}), autils.queryHostVersion.bind({hosts:state.hosts}))
 
-        // Add
-
-        diff.add.forEach(function(container) {
-            utils.queryHostVersion(container.host, function(version, _path) {
-                container.host = utils.stringifyHost(container.host)
-                var __path     = path.resolve(__dirname,_path)
-                var run        = cdi.run(container, { exclude : ['scale'] })[0].replace('docker', __path)
-                scheduler.exec(run, finish_checker) 
-            }) 
-        }) 
-
-        // Remove
-
-        diff.remove.forEach(function(container) {
-            var host = utils.pickHostByName(container.host, state.hosts) 
-            utils.queryHostVersion(host, function(version, _path) {
-                container.host = utils.stringifyHost(host)
-                var __path     = path.resolve(__dirname,_path)
-                var kill       = cdi.kill(container)[0].replace('docker', __path)
-                var rm         = cdi.rm(container)[0].replace('docker', __path)
-                scheduler.exec(kill, function() {
-                    scheduler.exec(rm, finish_checker) 
-                }) 
+        async.map(clone(diff.remove), kill, function(err, results) {
+            if (err) { scheduler.handleError(err); return }
+            async.map(clone(diff.remove), rm, function(err, results) {
+                if (err) { scheduler.handleError(err); return }
+                async.map(clone(diff.add), run, function(err, results) {
+                    if (err) { scheduler.handleError(err); return }
+                    scheduler.working = false
+                    // console.log(results)
+                })
             })
         })
+    },
+
+    handleError : function(err) {
+        console.error(err)
+        setTimeout(function() {
+            scheduler.working = false
+        },300000) // Give the cluster some time to recover and don't flood the logs
     }
 
 }
