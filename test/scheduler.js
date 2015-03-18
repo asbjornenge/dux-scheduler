84ce811cec239c18aa9e58d23ff8ed614b5250d9
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-03-18 12:45:20 +0100

    Added solution idea

--- test/scheduler.js
+++ test/scheduler.js
@@ -2,3 +2,7 @@ var assert    = require('assert')
 var scheduler = require('../scheduler')
 
 console.log(scheduler.diff)
+
+// This thing to do is to make sure there are nothing missing
+// in the current running config.
+// Altså ikke gjør en ordinær diff, bare sjekk at alt som er definer i config er med!
